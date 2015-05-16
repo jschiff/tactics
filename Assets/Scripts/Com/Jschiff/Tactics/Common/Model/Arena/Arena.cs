5b4184f171807bb4ae3f88054406304cd6ea9880
Jeremy Schiff <jschiff90@gmail.com>
2015-05-16 12:39:34 -0400

    Initial commit

---
+++ Assets/Scripts/Com/Jschiff/Tactics/Common/Model/Arena/Arena.cs
@@ -0,0 +1,18 @@
+namespace Com.Jschiff.Tactics.Common.Model.Arena
+{
+  public class Arena {
+    public Terrain Terrain {
+      get;
+      private set;
+    }
+    
+    public Effects Effects {
+      get;
+      private set;
+    }
+  
+    public Arena () {
+    }
+  }
+}
+
---
+++ Assets/Scripts/Com/Jschiff/Tactics/Common/Model/Arena/Terrain.cs
@@ -0,0 +1,11 @@
+namespace Scripts.Com.Jschiff.Tactics.Common.Model.Arena
+{
+  public class Terrain {
+  
+    Tile[,] tiles;
+  
+    public Terrain () {
+    }
+  }
+}
+
